60a919e5fb7c49202307c7126abedcdf4d25fd22
GreenRou <denis.kramar15@gmail.com>
2026-05-25 09:36:14 +0300

    build: increase version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.greenrou.kanata"
         minSdk = 28
         targetSdk = 36
-        versionCode = 5
-        versionName = System.getenv("VERSION_NAME")?.removePrefix("v") ?: "0.2.3"
+        versionCode = 6
+        versionName = System.getenv("VERSION_NAME")?.removePrefix("v") ?: "0.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
